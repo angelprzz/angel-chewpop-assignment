9e4ec49a8a56d35746307dfc7138a0fe58277adf
Angel Perez <angelpxrz@gmail.com>
2022-04-24 17:03:17 -0500

    Feat: Add Bottom Sheet package

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useRef } from "react";
+import { BottomSheet } from "react-spring-bottom-sheet";
+import "react-spring-bottom-sheet/dist/style.css";
+
+import logo from "./assets/popchew-logo.png";
+import "./index.css";
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const focusRef = useRef();
+  const sheetRef = useRef();
+
   return (
     <main className="app-main">
-      <p className="star">You got this! ⭐️</p>
-      <p className="confidential">
-        This content is confidential property of Popchew.
-      </p>
+      <img className="logo" src={logo} alt="Popchew Logo" />
+      <p className="h1">Welcome back, Rushir!</p>
+      <BottomSheet
+        open
+        skipInitialTransition
+        ref={sheetRef}
+        initialFocusRef={focusRef}
+        defaultSnap={({ maxHeight }) => maxHeight / 15}
+        snapPoints={({ maxHeight }) => [maxHeight / 15, maxHeight * 0.7]}
+      >
+        <p className="h2">Menu</p>
+      </BottomSheet>
     </main>
   );
 }
