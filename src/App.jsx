2ad63c74f29da9279cb6bcbd0313a5b95a91c1de
Angel Perez <angelpxrz@gmail.com>
2022-04-24 16:23:10 -0500

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app-main">
+      <p className="star">You got this! ⭐️</p>
+      <p className="confidential">
+        This content is confidential property of Popchew.
+      </p>
+    </main>
+  );
+}
+
+export default App;
